5f20f110ce55f3021fa1f45b4e6f29b13a2bb903
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-01 18:13:47 +0100

    Update export.py
    Signed-off-by: IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>

--- export.py
+++ export.py
@@ -1,12 +1,1 @@
-fi = open('tempo', 'r')
-mod = fi.readline()
-fi.close()
-
-if mod == 'pyb':
-    import pyb
-if mod == 'machine':
-    import machine
-if mod == 'network':
-    import network
-if mod == 'uasyncio':
-    import uasyncio
+f
