e239843b47bc3b44d168b3d8c363abdfb7f50301
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-04 20:22:30 +0100

    Create example.py
    Signed-off-by: IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>

---
+++ example.py
@@ -0,0 +1,6 @@
+##you have to import GIGA1.py in your boot.py file to start with it, without help of any other computer!
+##on your ucomputer, with your own keyboard, you can view a ligne of a file, copy a file or modify a file with the help of shell_commands.py.
+
+sc.view('boot.py',2)
+sc.cp('main.py', 'test.py', 3, 'name=bob')
+import test    #if you have add the ligne in your GIGA.py to import test.py
