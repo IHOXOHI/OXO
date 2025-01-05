1b76f7151bdf51e6575ac97a8e0cc8bc94e5f0e8
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-05 12:20:49 +0100

    Update example.py
    Signed-off-by: IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
-##you have to import GIGA1.py in your boot.py file to start with it, without help of any other computer!
-##on your ucomputer, with your own keyboard, you can view a ligne of a file, copy a file or modify a file with the help of shell_commands.py.
+##you have to import GIGA1.py from your boot.py file to start with it, without help of any other computer!
+##on your Ucomputer, with your own keyboard, you can 'print a file', copy a file or modify it with the help of shell_commands.py.
 
 sc.view('boot.py',2)
 sc.cp('main.py', 'test.py', 3, 'name=bob')
