e23c3dea96555e3404cc37c27d6c3fe3ee11f905
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-06 15:45:41 +0100

    Update GIGA1.py
    Signed-off-by: IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>

--- GIGA1.py
+++ GIGA1.py
@@ -1,57 +1,78 @@
+#importants libs
 import machine
 import pyb
-from machine import I2C, Pin, SPI
+from micropython import const
+#display
 from ST77352 import TFT
 from seriffont import seriffont
-from time import sleep_ms
-import os
+#optional
 import shell_commands as sc
 import uasyncio
 
-spi = SPI(1, baudrate=20000000)
-tft=TFT(spi,Pin('PG9'),Pin('PE5'),Pin('PG7'))
+# display dimensions:
+#width = const(128) #no necessary for moment
+#height = const(128) #no necessary for moment
+height_step = const(15)
+step_start = const(30)
+
+spi = machine.SPI(1, baudrate=20000000)
+tft=TFT(spi,machine.Pin('PG9'),machine.Pin('PE5'),machine.Pin('PG7'))
 tft.initr()
 tft.rotation(0)
 
-name_session= "@GIGA1:"
 punto = 0
 texto = ""
 modo = 0
 
 async def tft_display(texto):
     global punto, modo
+    tft.fill(tft.WHITE)
     if punto:
         texto = str(texto) + "|"
         punto = 0
     else:
         texto = str(texto)
         punto = 1
-    tft.fill(tft.WHITE)
-    tft.text((5, 30), name_session,tft.BLACK, seriffont, 2, nowrap=True)
-    tft.text((15, 80), texto,tft.BLACK, seriffont, 2, nowrap=True)
+    if len(texto) > height_step: #height_step is the same of (128 / space_letter = 15 in my case)
+        texto1 = texto[:height_step]
+        tft.text((5, step_start), texto1,tft.BLACK, seriffont, 2, nowrap=True)
+        texto2 = texto[height_step:]
+        tt = step_start + height_step
+        tft.text((5, tt), texto2,tft.BLACK, seriffont, 2, nowrap=True)
+    if len(texto) > (height_step*2):
+        texto1 = texto[:height_step]
+        tft.text((5, step_start), texto1,tft.BLACK, seriffont, 2, nowrap=True)
+        tt = step_start + height_step
+        texto2 = texto[height_step:tt]
+        tft.text((5, tt), texto2,tft.BLACK, seriffont, 2, nowrap=True)
+        tt = tt + height_step
+        texto3 = texto[tt:]
+        tft.text((5, tt), texto3,tft.BLACK, seriffont, 2, nowrap=True)
+    else:
+        tft.text((5, step_start), texto,tft.BLACK, seriffont, 2, nowrap=True)
 
 #Keyboard right
-Penter = Pin('PJ12', Pin.IN, Pin.PULL_UP)
-Pmode = Pin('PG13', Pin.IN, Pin.PULL_UP)
-Pspace = Pin('PJ0', Pin.IN, Pin.PULL_UP)
-Pdel = Pin('PG12', Pin.IN, Pin.PULL_UP)
-Pp = Pin('PJ1', Pin.IN, Pin.PULL_UP)
+Penter = machine.Pin('PJ12', machine.Pin.IN, machine.Pin.PULL_UP)
+Pmode = machine.Pin('PG13', machine.Pin.IN, machine.Pin.PULL_UP)
+Pspace = machine.Pin('PJ0', machine.Pin.IN, machine.Pin.PULL_UP)
+Pdel = machine.Pin('PG12', machine.Pin.IN, machine.Pin.PULL_UP)
+Pp = machine.Pin('PJ1', machine.Pin.IN, machine.Pin.PULL_UP)
 #left
-P0 = Pin('PE5', Pin.IN, Pin.PULL_UP)
-P1 = Pin('PI11', Pin.IN, Pin.PULL_UP)
-P2 = Pin('PE4', Pin.IN, Pin.PULL_UP)
-P3 = Pin('PK0', Pin.IN, Pin.PULL_UP)
-P4 = Pin('PB2', Pin.IN, Pin.PULL_UP)
-P5 = Pin('PH15', Pin.IN, Pin.PULL_UP)
-P6 = Pin('PI13', Pin.IN, Pin.PULL_UP)
-P7 = Pin('PG10', Pin.IN, Pin.PULL_UP)
-P8 = Pin('PI10', Pin.IN, Pin.PULL_UP)
-P9 = Pin('PI15', Pin.IN, Pin.PULL_UP)
-P10 = Pin('PK7', Pin.IN, Pin.PULL_UP)
-P11 = Pin('PE6', Pin.IN, Pin.PULL_UP)
-P12 = Pin('PI14', Pin.IN, Pin.PULL_UP)
-P13 = Pin('PJ7', Pin.IN, Pin.PULL_UP)
-P14 = Pin('PJ6', Pin.IN, Pin.PULL_UP)
+P0 = machine.Pin('PE5', machine.Pin.IN, machine.Pin.PULL_UP)
+P1 = machine.Pin('PI11', machine.Pin.IN, machine.Pin.PULL_UP)
+P2 = machine.Pin('PE4', machine.Pin.IN, machine.Pin.PULL_UP)
+P3 = machine.Pin('PK0', machine.Pin.IN, machine.Pin.PULL_UP)
+P4 = machine.Pin('PB2', machine.Pin.IN, machine.Pin.PULL_UP)
+P5 = machine.Pin('PH15', machine.Pin.IN, machine.Pin.PULL_UP)
+P6 = machine.Pin('PI13', machine.Pin.IN, machine.Pin.PULL_UP)
+P7 = machine.Pin('PG10', machine.Pin.IN, machine.Pin.PULL_UP)
+P8 = machine.Pin('PI10', machine.Pin.IN, machine.Pin.PULL_UP)
+P9 = machine.Pin('PI15', machine.Pin.IN, machine.Pin.PULL_UP)
+P10 = machine.Pin('PK7', machine.Pin.IN, machine.Pin.PULL_UP)
+P11 = machine.Pin('PE6', machine.Pin.IN, machine.Pin.PULL_UP)
+P12 = machine.Pin('PI14', machine.Pin.IN, machine.Pin.PULL_UP)
+P13 = machine.Pin('PJ7', machine.Pin.IN, machine.Pin.PULL_UP)
+P14 = machine.Pin('PJ6', machine.Pin.IN, machine.Pin.PULL_UP)
 
 
 texta = ""
@@ -59,11 +80,11 @@ champs1 = ""
 champs2 = ""
 moda = 1
 prime = 0
-Pprime = Pin('PA7', Pin.OUT)
+Pprime = machine.Pin('PA7', machine. Pin.OUT)
 
 async def check_keyboard():
     global texto, moda, prime
-    liste1 = ["sc.view('tempo.py', 3)","sc.cp('tempo.py', 'prout')","sc.view('prout', 3)",'d','e','f','g','h','i','j','k','l','m','/',',']
+    liste1 = ["sc.view('tempo.py', 5)","sc.cp('tempo.py', 'prout.py','3','        prout')","sc.view('prout.py', 179)",'d','e','f','g','h','i','j','k','l','m','/',',']
     liste2 = ["print('tempo.py')","sc.count('tempo.py')",'p','q','r','s','t','u','v','w','x','y','z',':','.']
     liste3 = ['0','1','2','3','4','5','6','7','8','9','+','-','*',"'","="]
     liste4 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','(',')']
@@ -122,8 +143,6 @@ async def enter(event):
     if texta == texto:
         texto = ""
     if texto[:5] == 'print': #less than 10 pages
-        fi = texto[6:-1]
-        print(fi)
         modo = 1
     if modo == 0:
         if texto[:6] == "import":
@@ -162,39 +181,43 @@ async def enter(event):
                 globals()[champs1] = champs2
 
         if texto != "":
-            print(texto)
+            print(texto, texto[:5])
+        #    if texto[:5] == 'sc.cp': # I don't understand why it works when I use repl and not there?
+         #       eval(texto)
+          #      texto = "Done."
+           # else:
             com = eval(texto)
             texto = com
         texta = texto
     await event.wait()
     event.clear()
 
-async def tft_display2(fi):#the name of the file dosen't pass???
+async def tft_display2():
     global modo, texto
     if modo == 1:
         modo = 2
-        #L2 = int(count)
+        L1 = 1
         L2 = 10
-        print('fi: '.format(fi))
+        file = 'tempo.py'
         fi = open('tempo.py', 'r')
         ligne = fi.readline()
         tft.fill(tft.WHITE)
-        nb = 1
         nl = 1
+        place = height_step
         while nl != L1:
             ligne = fi.readline()
         while nl != L2:
             ligne = str(nl) + ' ' + ligne
-            if len(ligne) > 15:
-                ligne1 = ligne[:15]
-                ligne2 = ligne[15:]
-                tft.text((5, nb), ligne1,tft.BLACK, seriffont, 2, nowrap=True)
-                nb += 15
-                tft.text((5, nb), ligne2,tft.BLACK, seriffont, 2, nowrap=True)
+            if len(ligne) > height_step:
+                ligne1 = ligne[:height_step]
+                tft.text((5, place), ligne1,tft.BLACK, seriffont, 2, nowrap=True)
+                ligne2 = ligne[height_step:]
+                place = place + height_step
+                tft.text((5, place), ligne2,tft.BLACK, seriffont, 2, nowrap=True)
             else:
-                tft.text((5, nb), ligne,tft.BLACK, seriffont, 2, nowrap=True)
+                tft.text((5, place), ligne,tft.BLACK, seriffont, 2, nowrap=True)
+                place = place + height_step
             ligne = fi.readline()
-            nb += 15
             nl += 1
         fi.close()
     if modo == 2:
@@ -208,7 +231,7 @@ async def main():
         if modo == 0:
             uasyncio.create_task(tft_display(texto))
         else:
-            uasyncio.create_task(tft_display2(fi))
+            uasyncio.create_task(tft_display2())
         uasyncio.create_task(check_keyboard())
         await uasyncio.sleep_ms(500)
 uasyncio.run(main())
