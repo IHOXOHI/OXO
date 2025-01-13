6d6e09a5990cb19bbf7c22de57da48b340b963f5
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-13 12:18:29 +0100

    Update example.py
    Signed-off-by: IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>

--- example.py
+++ example.py
@@ -1,6 +1,12 @@
-##you have to import GIGA1.py from your boot.py file to start with it, without help of any other computer!
-##on your Ucomputer, with your own keyboard, you can 'print a file', copy a file or modify it with the help of shell_commands.py.
+##you have to rename GIGA1.py main.py to start with it, without help of any other computer!
+##on your Ucomputer, with your own keyboard, manipulate variables, files, and import them, you can 'print a file', copy a file or modify it with the help of shell_commands.py.
+examples of commands:
+a = 5
+a ###it return 5
 
-sc.view('boot.py',2)
+file = open('myfile', 'w')...
+
 sc.cp('main.py', 'test.py', 3, 'name=bob')
-import test    #if you have add the ligne in your GIGA.py to import test.py
+sc.view #you have to indicate the name and which lignes in your main.py
+
+import test    #you have to add the ligne in your main.py, or place the file in your lib, to execute the file test.py
