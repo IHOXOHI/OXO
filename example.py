9f2d420b1a3a70fd631f9c8ddeaf3f28aa841503
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-13 12:19:31 +0100

    Update example.py
    Signed-off-by: IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 ##you have to rename GIGA1.py main.py to start with it, without help of any other computer!
-##on your Ucomputer, with your own keyboard, manipulate variables, files, and import them, you can 'print a file', copy a file or modify it with the help of shell_commands.py.
+##on your Ucomputer, with your own keyboard, you can manipulate variables, 'print a file', copy a file or modify it with the help of shell_commands.py.
 examples of commands:
 a = 5
 a ###it return 5
