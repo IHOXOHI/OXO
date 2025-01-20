cc6fde09f5f34dfef7f68fd42bbc52c073e49b9e
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-20 21:59:57 +0100

    Update example.py
    Signed-off-by: IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 ##you have to rename GIGA1.py main.py to start with it, without help of any other computer!
-##on your Ucomputer, with your own keyboard, you can manipulate variables, 'print a file', copy a file or modify it with the help of shell_commands.py.
+##on your Ucomputer, with your own keyboard, you can manipulate variables, 'display a part of a file', copy a file or modify it with the help of shell_commands.py.
 examples of commands:
 a = 5
 a ###it return 5
 
 file = open('myfile', 'w')...
 
-sc.cp('main.py', 'test.py', '3', 'name=bob') # copy main.py in test.py and change the line 3 by name=bob
-sc.view #you have to indicate the name and which lignes in your main.py... you can do it with a sc.cp command
+sc.cp('main.py', 'test.py', 3, 'name=bob') # copy main.py in test.py and change the line 3 by name=bob
+sc.view #you have to indicate the name and which lignes in your main.py... you can do it with a sc.md command and reset
 
 import test    #you have to place the file in your lib and add the line to indicate this possible import in your main (you can too import directly a module by your main), to execute the file test.py
 
-Until now, no if, for, while, ..., functions, ...
+NO if, for, while, ..., functions, ...
