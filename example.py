ebf6465008c5ac1d256f7d9cca718fea4971908e
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-21 10:57:50 +0100

    Update example.py
    Signed-off-by: IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ file = open('myfile', 'w')...
 sc.cp('main.py', 'test.py', 3, 'name=bob') # copy main.py in test.py and change the line 3 by name=bob
 sc.md('boot.py', 5, "pyb.main('script.py'")
 aa2 ###display the command passed before the last one
-bb #display the file named at the line 188; you can change this name by a sc.md command, press reset, and after write 'bb'
+bb #display the lines of the file named at the line 188 and 189; you can change the name or the line by a sc.md command, press reset, and after write 'bb'
 cc # list alls files
 
 import myscript    #you have to place the file in your lib and add the line to indicate this possible import in your main (you can too import directly a module by your main), to execute the file test.py
