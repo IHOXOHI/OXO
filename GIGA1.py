05ca8604f3b5787844370b8ffceedefb59a0f62d
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-01-21 10:45:34 +0100

    Update main.py
    Signed-off-by: IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>

--- GIGA1.py
+++ GIGA1.py
@@ -4,6 +4,7 @@ import uasyncio
 from ssd1306 import SSD1306_I2C
 import shell_commands as sc
 from micropython import const
+#import myscript
 
 i2c = machine.I2C(1)
 oled = SSD1306_I2C(128,64,i2c)
@@ -89,11 +90,11 @@ moda = 1
 async def check_keyboard():
     global texto, moda, prime
     liste1 = ['a','b','c','d','e','f','g','h','i','j','k','l','m','/',',']
-    liste2 = ["sc.view",'o','p','q','r','s','t','u','v','w','x','y','z',':','.']
+    liste2 = ['n','o','p','q','r','s','t','u','v','w','x','y','z',':','.']
     liste3 = ['0','1','2','3','4','5','6','7','8','9','+','-','*',"'","="]
     liste4 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','(',')']
     liste5 = ['N','O','P','Q','R','S','T','U','V','W','X','Y','Z','[',']']
-    liste6 = [';','!','?','>','<','#','_','{','}','\"',"sc.md('main.py',188,\"        fil='IFI.py'\")",'`','%','', "sc.md('main.py', '167', '        L1,L2 = 3,6')"]
+    liste6 = ["sc.md('boot.py',5,\"pyb.main('SCRIPT.py')\")",'!','!','>','<','#','_','{','}','\"',"sc.md('main.py',188,\"        fil='SCRIPT.py'\")",'`','%','', "sc.md('main.py', 167, '        L1,L2 = 3,6')"]
     if Penter.value() == 0:
         event = uasyncio.Event()
         uasyncio.create_task(enter(event))
@@ -119,7 +120,6 @@ async def check_keyboard():
     for i in range(15):
         key = eval('P' + str(i) + ".value()")
         if key == 0:
-            print(key)
             if moda == 1:
                 if prime:
                     texto = texto + liste4[i]
@@ -136,21 +136,30 @@ async def check_keyboard():
                 else:
                     texto = texto + liste3[i]
 modo = 0
+reci = ['1','2','3','4','5'] # five records of passed commands
 async def enter(event):
-    global texto, texta, champs1, champs2, modo
+    global texto, texta, champs1, champs2, modo, reci
     texto = str(texto)
-    print(texto)
     if texta == texto:
         texto = ""
-    if texto[:7] == 'sc.view':
+    if modo == 3:
+        modo == 0
+    if texto[:2] == 'aa': #you have to write 'aa1' for the last command
+        qq = 5 - (int(texto[2:]))
+        texto = reci[qq]
+        modo = 3
+    if texto[:2] == 'bb': # to display a file named at the line 188
         texto = ""
         modo = 1
+    if texto[:2] == 'cc': # to list alls files
+       import os
+       texto = os.listdir()
     if modo == 0:
         if texto[:6] == "import":
             textu = texto[7:]
             try:
-                if textu == 'redi':
-                    import redi
+                if textu == 'myscript':
+                    import myscript
                 texto = ""
             except:
                 texto = 'no modules named: ' + textu
@@ -171,12 +180,19 @@ async def enter(event):
                     champs2 = int(champs2)
                 except:
                     pass
+                reci.append(texto)
+                reci.pop(0)
                 texto = ""
                 globals()[champs1] = champs2
         texta = texto
         if texto != "":
-            com = eval(texto)
-            texto = com
+            try:
+                com = eval(texto)
+                texto = com
+            except TypeError:
+                pass
+            except:
+                texto = 'ERROR'
     await event.wait()
     event.clear()
 
@@ -185,7 +201,7 @@ async def oled_display2():
     if modo == 1:
         modo = 2
         L1, L2 = 1,3
-        fil='IFI.py'
+        fil = 'tempo.py'
         fi = open(fil, 'r')
         ligne = fi.readline()
         oled.fill(0)
@@ -216,7 +232,7 @@ async def oled_display2():
             await uasyncio.sleep_ms(200)
 async def main():
     while 1:
-        if modo == 0:
+        if modo == 0 or modo == 3:
             uasyncio.create_task(oled_display(texto))
         if modo == 1:
             uasyncio.create_task(oled_display2())
