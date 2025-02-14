0eddaaa127b90e02efb6b1e22547afc73ab69b06
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2025-02-14 11:23:01 +0100

    Update shell_commands.py
    the cp_view permite to see a file witout reset
    
    Signed-off-by: IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>

--- shell_commands.py
+++ shell_commands.py
@@ -1,12 +1,25 @@
+import os
+
+def view(ori,L=1):
+    fil_ori = open(ori, "r")
+    if L != 1:
+        n = 1
+        L -= 1
+        while n != L:
+            l = fil_ori.readline()
+            n+=1
+    l = fil_ori.readline()
+    fil_ori.close()
+    return l
 
 def cp(ori, des,ch_lg=0, ch_txt="\n"):
     fil_ori = open(ori, "r")
     fil_des = open(des, "w")
     l = fil_ori.readline()
     n = 1
-    ch_txt = ch_txt + '\n' + "\n"
+    ch_txt = ch_txt + '\n'
     while l != "":
-        if str(n) == ch_lg:
+        if n == int(ch_lg):
             text = str(ch_txt)
         else:
             text = str(l)
@@ -27,24 +40,23 @@ def count(ori):
     fil_ori.close()
     return str(n)
 
-def md(ori,ch_lg=0, ch_txt="\n"):
+def md(ori,ch_lg=0, ch_txt=""):
     fil_ori = open(ori, "r")
-    fil_des = open('temp_fil.py', "w")
+    fil_des = open('tempo.py', "w")
     l = fil_ori.readline()
     n = 1
     while l != "":
         if n == int(ch_lg):
-            text = str(ch_txt) +  "\n" + "\n" ## it works like this????
-            fil_des.write(text)
+            text = str(ch_txt)
         else:
             text = str(l)
-            fil_des.write(text)
+        fil_des.write(text)
         l = fil_ori.readline()
         n += 1
     fil_ori.close()
     fil_des.close()
 
-    fil_ori = open('temp_fil.py', "r")
+    fil_ori = open('tempo.py', "r")
     fil_des = open(ori, "w")
     l = fil_ori.readline()
     n = 1
@@ -59,5 +71,21 @@ def md(ori,ch_lg=0, ch_txt="\n"):
     fil_ori.close()
     fil_des.close()
 
-    os.remove('temp_fil.py')
-    return 1
+    os.remove('tempo.py')
+    return "ok"
+
+def cp_view(ori, L1=1, L2=3):
+    fil_ori = open(ori, "r")
+    fil_des = open('tempo.py', "w")
+    l = fil_ori.readline()
+    n = 1
+    while n != L1:
+        l = fil_ori.readline()
+        n += 1
+    while n != L2:
+        text = l + "\n"
+        fil_des.write(text)
+        l = fil_ori.readline()
+        n += 1
+    fil_ori.close()
+    fil_des.close()
